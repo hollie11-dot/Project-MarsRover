74c342325da5fb96fce60523cfa6caa4ab4464ad
Hollie <holliemount11@hotmail.com>
2024-11-14 17:09:52 +0000

    Feat: create PlateauSize class

---
+++ src/main/java/PlateauSize.java
@@ -0,0 +1,12 @@
+public class PlateauSize {
+//    private final int[][] gridSize = new int[5][5];
+
+    private int x;
+    private int y;
+
+    public void setSize(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
