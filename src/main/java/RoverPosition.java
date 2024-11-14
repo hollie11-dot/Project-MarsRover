7d1d5430869c7c1e72c32d702e4d531bb9c907bb
Hollie <holliemount11@hotmail.com>
2024-11-14 17:10:18 +0000

    Feat: Create RoverPosition class

---
+++ src/main/java/RoverPosition.java
@@ -0,0 +1,18 @@
+public class RoverPosition {
+    private int x;
+    private int y;
+    private CompassDirection facing;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public CompassDirection getFacing() {
+        return facing;
+    }
+
+}
