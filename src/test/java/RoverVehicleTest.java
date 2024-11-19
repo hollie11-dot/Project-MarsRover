66c7508e415a45d22157b9f2cdd3cced4df6ca27
Hollie <holliemount11@hotmail.com>
2024-11-19 08:17:00 +0000

    Test: implement testRotateTurnsRight

--- src/test/java/RoverVehicleTest.java
+++ src/test/java/RoverVehicleTest.java
@@ -25,24 +25,23 @@ class RoverVehicleTest {
         assertEquals(roverVehicle.getDirection(), CompassDirection.N);
     }
 
-//    @Test
-//    @DisplayName("Turns left based on input direction")
-//    void testRotateTurnsRight() {
-//    //Arrange
-//    RoverVehicle testCase = new RoverVehicle(new RoverPosition(2, 5, CompassDirection.E), "Rover2");
-//    CompassDirection testDirection = CompassDirection.N;
-//        CompassDirection testDirection2 = CompassDirection.E;
-//        CompassDirection testDirection3 = CompassDirection.S;
-//        CompassDirection testDirection4 = CompassDirection.W;
-//
-//    Instruction testInstruction = Instruction.R;
-//        Assertions.assertAll(
-//                "Grouped assertions of instructions to turn right",
-//                () -> assertEquals(CompassDirection.E, testCase.rotate(testDirection, testInstruction)),
-//                () -> assertEquals(CompassDirection.S, testCase.rotate(testDirection2, testInstruction)),
-//                () -> assertEquals(CompassDirection.W, testCase.rotate(testDirection3, testInstruction)),
-//                () -> assertEquals(CompassDirection.N, testCase.rotate(testDirection4, testInstruction))
-//        );
-//}
+    @Test
+    @DisplayName("Turns right based on input direction")
+    void testRotateTurnsRight() {
+        //Arrange
+        RoverVehicle roverVehicle = new RoverVehicle(new RoverPosition(1, 3, CompassDirection.N), "Rover2");
+
+        roverVehicle.rotate(Instruction.R);
+        assertEquals(roverVehicle.getDirection(), CompassDirection.E);
+
+        roverVehicle.rotate(Instruction.R);
+        assertEquals(roverVehicle.getDirection(), CompassDirection.S);
+
+        roverVehicle.rotate(Instruction.R);
+        assertEquals(roverVehicle.getDirection(), CompassDirection.W);
+
+        roverVehicle.rotate(Instruction.R);
+        assertEquals(roverVehicle.getDirection(), CompassDirection.N);
+    }
 
 }
