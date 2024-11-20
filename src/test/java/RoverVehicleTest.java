433390fe8bb41c0a1e6b9378e7f82fa474f7a0b9
Hollie <holliemount11@hotmail.com>
2024-11-20 16:48:51 +0000

    Test: implement tests for move method

--- src/test/java/RoverVehicleTest.java
+++ src/test/java/RoverVehicleTest.java
@@ -44,4 +44,35 @@ class RoverVehicleTest {
         assertEquals(roverVehicle.getDirection(), CompassDirection.N);
     }
 
+    @Test
+    @DisplayName("Test that Y co-ordinate increments when Rover moves forward from North facing position")
+    void testMoveGoesNorth (){
+        RoverVehicle roverVehicle = new RoverVehicle(new RoverPosition(1, 3, CompassDirection.N), "movingRover");
+        roverVehicle.move(Instruction.M);
+        assertEquals(4, roverVehicle.getYCoordinates());
+    }
+
+    @Test
+    @DisplayName("Test that X co-ordinate increments when Rover moves forward from East facing position")
+    void testMoveGoesEast (){
+        RoverVehicle roverVehicle = new RoverVehicle(new RoverPosition(1, 3, CompassDirection.E), "movingRover");
+        roverVehicle.move(Instruction.M);
+        assertEquals(2, roverVehicle.getXCoordinates());
+    }
+
+    @Test
+    @DisplayName("Test that X co-ordinate decrements when Rover moves forward from West facing position")
+    void testMoveGoesWest (){
+        RoverVehicle roverVehicle = new RoverVehicle(new RoverPosition(2, 3, CompassDirection.W), "movingRover");
+        roverVehicle.move(Instruction.M);
+        assertEquals(1, roverVehicle.getXCoordinates());
+    }
+
+    @Test
+    @DisplayName("Test that Y co-ordinate decrements when Rover moves forward from South facing position")
+    void testMoveGoesSouth (){
+        RoverVehicle roverVehicle = new RoverVehicle(new RoverPosition(2, 3, CompassDirection.S), "movingRover");
+        roverVehicle.move(Instruction.M);
+        assertEquals(2, roverVehicle.getYCoordinates());
+    }
 }
